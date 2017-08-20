3256dc8fde873516b558baa37f504eeb2431dc94
cookieshake <cookieshake@hotmail.com>
2017-08-21 02:12:52 +0900

    add c&d model

--- cats_vs_dogs/models.py
+++ cats_vs_dogs/models.py
@@ -12,6 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import math
+
+import tensorflow as tf
+import utils
+
+import tensorflow.contrib.slim as slim
+from tensorflow.contrib.slim.python.slim.nets import resnet_v2, vgg, overfeat, alexnet
+
+
 """Contains the base class for models."""
 
 class BaseModel(object):
@@ -19,3 +28,9 @@ class BaseModel(object):
 
   def create_model(self, unused_model_input, **unused_params):
     raise NotImplementedError()
+
+class ResNetModel(BaseModel):
+  def create_model(self, model_input, vocab_size=2, **unused_params):
+    output = resnet_v2.resnet_v2_50(model_input, num_classes=vocab_size, is_training=False)[1]['predictions']
+    output = tf.reshape(output, [-1, num_classes])
+    return {"predictions": output}
